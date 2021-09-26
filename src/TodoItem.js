58e5bd10380a5bfd5ac5a2fd1ef736c61f5466bb
PabloMendozaxd <jose.mendozaji@my.unitec.edu.mx>
2021-09-26 17:41:09 -0500

    onSearchValueChange event

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,16 +2,18 @@ import React from 'react'
 import './TodoItem.css'
 
 function TodoItem(props) {
+  const onComplete=()=>alert(`Completaste la tarea ${props.text}`);
+  const onDelete=()=>alert(`Borraste la tarea ${props.text}`);
     return (
         <li className="TodoItem">
-      <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
+      <span onClick={onComplete} className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
         ✅
       </span>
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">
-        X
+      <span onClick={onDelete} className="Icon Icon-delete">
+        ❌
       </span>
     </li>
     )
